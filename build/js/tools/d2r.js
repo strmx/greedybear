60eb9084fcda770c6c30366fc8c5d6343d014c61
m.strasinskis <m.strasinskis@zeros.ones.de>
2016-01-31 23:20:59 +0100

    - removed build/js from git

--- build/js/tools/d2r.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    function d2r(d) {
-        var n = d * (Math.PI / 180);
-        return Math.round(n * 1000) / 1000;
-    }
-    return d2r;
-});
