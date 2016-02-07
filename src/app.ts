4d677cd3c58919efa50fca2ccdfb94a003f49d12
m.strasinskis <m.strasinskis@zeros.ones.de>
2016-02-07 14:29:05 +0100

    added Vector2 cell based movement

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import KeyboardInput = require('./KeyboardInput');
 import enums = require('./enums');
 import d2r = require('./tools/d2r');
 
+const V2 = BABYLON.Vector2;
+const V3 = BABYLON.Vector3;
 
+const ANGLE_RIGHT = 0;
+const ANGLE_BOTTOM = 1.5707963267948966;
+const ANGLE_LEFT = 3.141592653589793;
+const ANGLE_TOP = -1.5707963267948966;
 
 //////////////////////
 // MAP GENERATION
@@ -92,16 +98,9 @@ engine.runRenderLoop(() => {
 });
 
 
-
-// var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-var freeCamera = new BABYLON.FreeCamera("freeCamera", new BABYLON.Vector3(n/2, Math.min(n, m), m/2), scene);
-freeCamera.setTarget(new BABYLON.Vector3(n/2, 0, m/2));
-freeCamera.attachControl(canvas.element, true);
-
-
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 10, 0), scene);
-// var light = new BABYLON.DirectionalLight("Dir0", new BABYLON.Vector3(0, -1, 0), scene);
+var light = new BABYLON.HemisphericLight("light1", new V3(0, 10, 0), scene);
+// var light = new BABYLON.DirectionalLight("Dir0", new V3(0, -1, 0), scene);
 // light.diffuse = new BABYLON.Color3(1, 1, 1);
 // light.specular = new BABYLON.Color3(1, 1, 1);
 
@@ -160,7 +159,7 @@ positions.slice(1).forEach(p => {
   clone.position.x = p.x;
   clone.position.z = p.y;
   let size =p.distance / biggestAppleSize;
-  // clone.scaling = new BABYLON.Vector3(size, size, size);
+  // clone.scaling = new V3(size, size, size);
 });
 
 //
@@ -177,62 +176,177 @@ let agentPos = positions[0];
 agentOrigin.position.x = agentPos.x;
 agentOrigin.position.z = agentPos.y;
 
-
 //
 // agent camera
 //
 let ____AGENTCAMERA____;
 
 let pos = agentOrigin.position;
-var agentCamera = new BABYLON.FreeCamera("agentCamera", new BABYLON.Vector3(pos.x, 15, pos.z), scene);
-agentCamera.setTarget(agentOrigin.position);
+// var agentCamera = new BABYLON.FollowCamera("agentCamera", new V3(pos.x, 10, pos.z), scene);
+var agentCamera = new BABYLON.FollowCamera("agentCamera", new V3(pos.x - 3, 10, pos.z - 1), scene);
+agentCamera.target = agentOrigin;
+agentCamera.radius = 8; // how far from the object to follow
+agentCamera.heightOffset = 10; // how high above the object to place the camera
+agentCamera.rotationOffset = 270; // the viewing angle
+agentCamera.cameraAcceleration = 0.025 // how fast to move
+agentCamera.maxCameraSpeed = 2 // speed limit
+
 scene.activeCamera = agentCamera;
-
-canvas.element.onclick = (e) => {
-  scene.activeCamera = scene.activeCamera !== agentCamera ? agentCamera : freeCamera;
-}
-
-
 
 //
 // controlls
 //
 let ____CONTROLLS____;
 
-let speed: BABYLON.Vector3 = new BABYLON.Vector3(1, 1, 1);
-let direction: BABYLON.Vector3 = new BABYLON.Vector3(1, 0, 0);
+let speed = 1;
+let speedV2 = new V2(speed, speed);
+let speedV3 = new V3(speed, speed, speed);
+let directionV2 = new V2(1, 0);
+let directionV3 = new V3(1, 0, 0);
+let rotation = agentOrigin.rotation;
+let aPos: BABYLON.Vector2 = new V2(agentOrigin.position.x, agentOrigin.position.z);
+let aCellPos: BABYLON.Vector2 = aPos.clone();
+let lastPressedNavigationKey = null;
 
+function rotateToLeft() {
+  directionV3 = new V3(-1, 0, 0);
+  directionV2 = new V2(-1, 0);
+  rotation.y = ANGLE_LEFT;
+  console.log(rotation.y, directionV3);
+}
+function rotateToBottom() {
+  directionV3 = new V3(0, 0, -1);
+  directionV2 = new V2(0, -1);
+  rotation.y = ANGLE_BOTTOM;
+  console.log(rotation.y, directionV3);
+}
+function rotateToRight() {
+  directionV3 = new V3(1, 0, 0);
+  directionV2 = new V2(1, 0);
+  rotation.y = ANGLE_RIGHT;
+  console.log(rotation.y, directionV3);
+}
+function rotateToTop() {
+  directionV3 = new V3(0, 0, 1);
+  directionV2 = new V2(0, 1);
+  rotation.y = ANGLE_TOP;
+  console.log(rotation.y, directionV3);
+}
 
 KeyboardInput.getObservable.forEach(key => {
   switch (key) {
-    case enums.KEYS.LEFT:
-      // this._head.rotation.addInPlace(new BABYLON.Vector3(0, d2r(-90), 0));
-      console.log('L');
-      direction = new BABYLON.Vector3(-1, 0, 0);
-      break;
-    case enums.KEYS.UP:
-      // this._head.rotation.addInPlace(new BABYLON.Vector3(d2r(-90),0, 0));
-      console.log('U');
-      direction = new BABYLON.Vector3(0, 0, 1);
-      break;
+    // navigation
     case enums.KEYS.RIGHT:
-      // this._head.rotation.addInPlace(new BABYLON.Vector3(0, d2r(90), 0));
-      console.log('R');
-      direction = new BABYLON.Vector3(1, 0, 0);
+      lastPressedNavigationKey = key;
+      break;
+    case enums.KEYS.LEFT:
+      lastPressedNavigationKey = key;
+      break;
+    // speed (TODO: remove for production)
+    case enums.KEYS.UP:
+      speedV3.addInPlace(new V3(.5, .5, .5));
       break;
     case enums.KEYS.DOWN:
-      // this._head.rotation.addInPlace(new BABYLON.Vector3(d2r(90), 0, 0));
-      console.log('D');
-      direction = new BABYLON.Vector3(0, 0, -1);
+      speedV3.subtractInPlace(new V3(.5, .5, .5));
       break;
+    default:
   }
 });
 
+function shiftAgent(tickDistance: number) {
+  let shift = new V2(tickDistance, tickDistance).multiply(directionV2);
+  let nextPos = aPos.add(shift);
+  let cellDiff = nextPos.subtract(aCellPos);
+  let fromPrevCellDistance = cellDiff.length();
+
+  if (fromPrevCellDistance < 1) {
+    aPos = nextPos;
+    agentOrigin.position.x = nextPos.x;
+    agentOrigin.position.z = nextPos.y;
+  } else {
+    //
+    // 1. move to next cell
+    //
+    let beforeNextCellDistance = 1 - aPos.subtract(aCellPos).length();
+    // used switch to use integers
+    switch(rotation.y) {
+      case ANGLE_RIGHT:
+        nextPos.x = aCellPos.x + 1;
+        break;
+      case ANGLE_BOTTOM:
+        nextPos.y = aCellPos.y - 1;
+        break;
+      case ANGLE_LEFT:
+        nextPos.x = aCellPos.x - 1;
+        break;
+      case ANGLE_TOP:
+        nextPos.y = aCellPos.y + 1;
+        break;
+    }
+    aCellPos.x = nextPos.x;
+    aCellPos.y = nextPos.y;
+
+    //
+    // apply navigation change (afte keypress)
+    //
+    if (lastPressedNavigationKey !== null) {
+      console.log('apply ', lastPressedNavigationKey);
+      switch (lastPressedNavigationKey) {
+        case enums.KEYS.LEFT:
+          switch(rotation.y) {
+            case ANGLE_RIGHT:
+              rotateToTop();
+              break;
+            case ANGLE_BOTTOM:
+              rotateToRight();
+              break;
+            case ANGLE_LEFT:
+              rotateToBottom();
+              break;
+            case ANGLE_TOP:
+              rotateToLeft();
+              break;
+          }
+          break;
+        case enums.KEYS.RIGHT:
+          switch(rotation.y) {
+            case ANGLE_RIGHT:
+              rotateToBottom();
+              break;
+            case ANGLE_BOTTOM:
+              rotateToLeft();
+              break;
+            case ANGLE_LEFT:
+              rotateToTop();
+              break;
+            case ANGLE_TOP:
+              rotateToRight();
+              break;
+          }
+          break;
+      }
+
+        lastPressedNavigationKey = null;
+    }
+
+    //
+    // TODO: check collision
+    // calculate future cell
+    // check availability on pattern
+
+    //
+    // move further
+    //
+    let remainDistance = fromPrevCellDistance - 1;
+    shiftAgent(remainDistance);
+  }
+}
+
+
 engine.runRenderLoop(() => {
   let sec = engine.getDeltaTime() / 1000;
-
-  let delta = speed.multiplyByFloats(sec, sec, sec).multiplyInPlace(direction);
-  agentOrigin.position.addInPlace(delta);
+  let distance = sec * speed;
+  shiftAgent(distance);
 });
 
 
@@ -247,6 +361,20 @@ engine.runRenderLoop(() => {
 //   () => {}
 // );
 
+
+//
+// all camera
+//
+let ____ALLCAMERA____;
+
+// var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+var freeCamera = new BABYLON.FreeCamera("freeCamera", new V3(n/2, Math.min(n, m), m/2), scene);
+freeCamera.setTarget(new V3(n/2, 0, m/2));
+freeCamera.attachControl(canvas.element, true);
+
+canvas.element.onclick = (e) => {
+  scene.activeCamera = scene.activeCamera !== agentCamera ? agentCamera : freeCamera;
+}
 
 
 // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
